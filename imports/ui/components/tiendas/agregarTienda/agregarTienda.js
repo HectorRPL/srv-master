5d87d3433186f297c9caf9e0acd61d208e8fe3f3
hEctor <hector.kaizen@gmail.com>
2017-04-04 16:22:27 -0500

    El modal de 'agregarTienda' ahora cuenta con validación por cada paso del wizard Es decir; el usuario no podrá avanzar hasta no completar correctamente cada paso.

--- imports/ui/components/tiendas/agregarTienda/agregarTienda.js
+++ imports/ui/components/tiendas/agregarTienda/agregarTienda.js
@@ -17,25 +17,9 @@ class AgregarTienda {
         this.pasoActual = 1;
         this.pasoAnterior = 0;
         this.datosFiscales = {};
-        this.datosFiscales = {
-            razonSocial: 'DEMO S.A. DE C.V.',
-            rfc: 'SLA630306CF7',
-            email: 'demo@demo.com',
-            estado: 'CIUDAD DE MEXICO',
-            estadoId: 'CMX',
-            delMpio: 'XOCHIMILCO',
-            codigoPostal: '16030',
-            colonia: 'POTRERO DE SAN BERNARDINO',
-            calle: 'ROSELINA',
-            numExt: '7',
-            numInt: '2'
+        this.datos = {
+            telefonos: [{telefono: ''}]
         };
-        this.datos =  {
-            nombre: 'DEMO',
-            email: 'DEMO01@DEMO01.COM',
-            telefonos: [{telefono: '5556769502'}]
-        };
-
     }
 
     agregarTelefono() {
@@ -49,18 +33,17 @@ class AgregarTienda {
 
     siguiente() {
         this.pasoAnterior = this.pasoActual;
-        this.pasoActual ++;
+        this.pasoActual++;
     }
 
     atras() {
-        this.pasoActual --;
+        this.pasoActual--;
         this.pasoAnterior = this.pasoActual - 1;
     }
 
-   agregar() {
+    agregar() {
         this.tipoMsj = '';
         // Inserta la tienda en la collection tiendas
-       console.log('Este es el objeto que vamos a enviar', this.datos);
         insertar.call(this.datos, this.$bindToContext((err, result)=> {
             if (err) {
                 this.msj = err.reason;
@@ -74,7 +57,7 @@ class AgregarTienda {
                 let datosFiscalesFinal = angular.copy(this.datosFiscales);
                 delete datosFiscalesFinal.colonias;
 
-                datosFiscalesFinal.proveedorId =  result;
+                datosFiscalesFinal.proveedorId = result;
                 insertarDatosFiscales.call(datosFiscalesFinal, this.$bindToContext((err)=> {
                     if (err) {
                         this.msj = err.reason;
@@ -90,7 +73,7 @@ class AgregarTienda {
         }));
     }
 
-    cerrar(){
+    cerrar() {
         this.dismiss();
     }
 
