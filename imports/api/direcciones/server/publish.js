6f4b449cac0c1ca948e7aeb35ce19e412f780d5d
jalevtz <j.vlt.mtz@gmail.com>
2017-04-07 12:52:52 -0500

    Publicacion encargada de mostrar una direccion

---
+++ imports/api/direcciones/server/publish.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jvltmtz on 15/09/16.
+ */
+import {Meteor} from "meteor/meteor";
+import {Direcciones} from "../collection";
+import {Candidatos} from "../../candidatos/collection";
+
+if (Meteor.isServer) {
+
+}
