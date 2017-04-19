fbff8c2c654b15366a7ee0756cb7a0aafe3d78f3
jalevtz <j.vlt.mtz@gmail.com>
2017-04-19 18:36:54 -0500

    Modificaciones para mostra los empleados de una tienda, agregar empleado

--- imports/ui/components/tiendas/admonTienda/personal/agregarEmpleado/agregarEmpleado.js
+++ imports/ui/components/tiendas/admonTienda/personal/agregarEmpleado/agregarEmpleado.js
@@ -2,13 +2,14 @@
  * Created by jvltmtz on 10/03/17.
  */
 import "./agregarEmpleado.html";
-import {name as Alertas} from "../../../comun/alertas/alertas";
-import {name as ElegirAnio} from "../../../comun/selects/elegirFechaNacimiento/elegirAnio/elegirAnio";
-import {name as ElegitMes} from "../../../comun/selects/elegirFechaNacimiento/elegirMes/elegirMes";
-import {name as ElegitDia} from "../../../comun/selects/elegirFechaNacimiento/elegirDia/elegirDia";
-import {name as FormaDireccion} from "../../../direccion/formaDireccion/formaDireccion";
-import {name as ElegirDepartamento} from "../../../comun/selects/elegirDepartamento/elegirDepartamento";
-import {insertar} from "../../../../../api/users/methods";
+import {name as Alertas} from "../../../../comun/alertas/alertas";
+import {name as ElegirAnio} from "../../../../comun/selects/elegirFechaNacimiento/elegirAnio/elegirAnio";
+import {name as ElegitMes} from "../../../../comun/selects/elegirFechaNacimiento/elegirMes/elegirMes";
+import {name as ElegitDia} from "../../../../comun/selects/elegirFechaNacimiento/elegirDia/elegirDia";
+import {name as FormaDireccion} from "../../../../direccion/formaDireccion/formaDireccion";
+import {name as ElegirDepartamento} from "../../../../comun/selects/elegirDepartamento/elegirDepartamento";
+import {insertarUsu} from "../../../../../../api/users/methods";
+import {insertar} from "../../../../../../api/direcciones/methods";
 
 class AgregarEmpleado {
     constructor($scope, $reactive, $state) {
@@ -26,31 +27,26 @@ class AgregarEmpleado {
     }
 
     siguiente() {
-        this.pasoAnterior = this.pasoActual;
         this.pasoActual++;
-    }
-
-    atras() {
-        this.pasoActual--;
-        this.pasoAnterior = this.pasoActual - 1;
+        this.msj = 'Paso de uno del registro completado.';
+        this.tipoMsj = 'success';
     }
 
     // Inserta usuario, empleado y dirreccion empleados
-    agregar() {
+    agregarUsuario() {
         this.tipoMsj = '';
         this.credentials.username = this.credentials.email;
         this.credentials.profile.direccion = this.direccion;
         this.credentials.profile.tiendaId = this.resolve.tiendaId;
 
-        insertar.call(this.credentials, this.$bindToContext((err, result)=> {
+        insertarUsu.call(this.credentials, this.$bindToContext((err, result)=> {
             if (err) {
-                console.log(err);
                 this.msj = err.reason;
                 this.tipoMsj = 'danger';
             } else {
-
-                this.msj = 'El sistema está trabajando, tardará unos minutos';
-                this.tipoMsj = 'warning';
+                this.pasoActual++;
+                this.msj = 'Registro del empleado se realizó con exito.';
+                this.tipoMsj = 'success';
 
             }
         }));
@@ -75,7 +71,7 @@ export default angular
         ElegirDepartamento
     ])
     .component(name, {
-        templateUrl: `imports/ui/components/tiendas/empleados/${name}/${name}.html`,
+        templateUrl: `imports/ui/components/tiendas/admonTienda/personal/${name}/${name}.html`,
         controllerAs: name,
         controller: AgregarEmpleado,
         bindings: {
--- imports/ui/components/tiendas/admonTienda/personal/personal.js
+++ imports/ui/components/tiendas/admonTienda/personal/personal.js
@@ -2,17 +2,47 @@
  * Created by Héctor on 14/04/2017.
  */
 import "./personal.html";
+import {Tiendas} from "../../../../../api/catalogos/tiendas/collection";
+import {Empleados} from "../../../../../api/empleados/collection";
+import {name as AgregarEmpleado} from './agregarEmpleado/agregarEmpleado';
 
-class Personal{
+class Personal {
     constructor($scope, $reactive, $state, $uibModal, $stateParams) {
         'ngInject';
         this.$state = $state;
         $reactive(this).attach($scope);
-        this.tiendaId = $stateParams.tiendaId;
-        console.log('vamos a imprimir el this.tiendaId', this.tiendaId);
         this.titulo = 'Tiendas';
+        this.tiendaId = $stateParams.tiendaId;
         this.$uibModal = $uibModal;
+        this.subscribe('tiendas.seleccionada', ()=> [{_id: this.tiendaId}]);
+        this.subscribe('empleados.porTienda', ()=> [{tiendaId: this.tiendaId}]);
 
+        this.helpers({
+            tienda(){
+                return Tiendas.findOne({_id: this.tiendaId});
+            },
+            empleados(){
+                return Empleados.find({tiendaId: this.tiendaId});
+            }
+        });
+
+    }
+
+    modalAgregar() {
+        const tiendaId = this.tiendaId;
+        var modalInstance = this.$uibModal.open({
+            animation: true,
+            component: 'AgregarEmpleado',
+            backdrop: 'static',
+            size: 'md',
+            keyboard: true,
+            resolve: {
+                tiendaId: function () {
+                    return tiendaId;
+                }
+
+            }
+        });
     }
 
 }
@@ -21,7 +51,9 @@ const name = 'personal';
 
 // create a module
 export default angular
-    .module(name, [])
+    .module(name, [
+        AgregarEmpleado
+    ])
     .component(name, {
         templateUrl: `imports/ui/components/tiendas/admonTienda/${name}/${name}.html`,
         controllerAs: name,
