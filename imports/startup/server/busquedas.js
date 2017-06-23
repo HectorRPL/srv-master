1ec9c179ba38c3c2abdf992fc53df794d127d541
jalevtz <j.vlt.mtz@gmail.com>
2017-06-23 14:32:27 -0500

    Se importan los metodos de busqueda al momento de cargar el servidor

--- imports/startup/server/busquedas.js
+++ imports/startup/server/busquedas.js
@@ -7,3 +7,5 @@ import "../../api/catalogos/codigosPostales/busquedas";
 import "../../api/catalogos/marcas/busquedas";
 import "../../api/catalogos/proveedores/busquedas";
 import "../../api/catalogos/productos/busquedas";
+import "../../api/catalogos/tiendas/busquedas";
+import "../../api/empleados/busquedas";
