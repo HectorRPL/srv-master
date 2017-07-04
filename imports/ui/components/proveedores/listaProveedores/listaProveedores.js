8dd6f5d26fd2354fc8ead8eb93f8ecf4862ce2e8
hEctor <hector.kaizen@gmail.com>
2017-07-03 19:04:31 -0500

    Se crea un modal para ver los detalles de los proveedores.

--- imports/ui/components/proveedores/listaProveedores/listaProveedores.js
+++ imports/ui/components/proveedores/listaProveedores/listaProveedores.js
@@ -3,6 +3,7 @@
  */
 import template from "./listaProveedores.html";
 import {Proveedores} from "../../../../api/catalogos/proveedores/collection";
+import {name as DetallesProveedor} from './detallesProveedor/detallesProveedor';
 import {name as AgregarProveedor} from '../agregarProveedor/agregarProveedor';
 import {name as EditarProveedor} from '../editarProveedor/editarProveedor';
 import {name as BuscarProveedor} from '../../comun/busquedas/buscarProveedor/buscarProveedor';
@@ -44,24 +45,31 @@ class ListaProveedores {
     }
 
 
-    modalAgregar() {
+    abreModalDealles(proveedor) {
         var modalInstance = this.$uibModal.open({
             animation: true,
-            component: 'AgregarProveedor',
+            component: 'DetallesProveedor',
             backdrop: 'static',
-            size: 'md',
+            size: 'xs',
             keyboard: true,
+            resolve: {
+                proveedor: function () {
+                    return proveedor;
+                }
+            }
+
         });
     }
+
 
 
 }
 
 const name = 'listaProveedores';
 
-// create a module
 export default angular
     .module(name, [
+        DetallesProveedor,
         AgregarProveedor,
         EditarProveedor,
         BuscarProveedor
