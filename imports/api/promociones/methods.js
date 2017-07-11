fddf2980b1b2b82772f3df22149cf153b76e19f2
jalevtz <j.vlt.mtz@gmail.com>
2017-07-11 13:54:16 -0500

    Se crea coleccion y metodos para promociones

---
+++ imports/api/promociones/methods.js
@@ -0,0 +1,109 @@
+/**
+ * Created by jvltmtz on 7/07/17.
+ */
+import {Meteor} from "meteor/meteor";
+import {DDPRateLimiter} from "meteor/ddp-rate-limiter";
+import {ValidatedMethod} from "meteor/mdg:validated-method"
+import {CallPromiseMixin} from "meteor/didericis:callpromise-mixin";
+import {_} from "meteor/underscore";
+import {Promociones} from "./collection";
+import {BitaFactoresPromo} from "../bitacoras/factoresPromo/collection";
+import {ProductosInventarios} from "../inventarios/productosInventarios/collection";
+
+const CAMPOS_PROMOCIONES = ['nombre', 'descuento', 'precioLista', 'fechaInicio', 'fechaFin'];
+
+
+export const altaPromocion = new ValidatedMethod({
+    name: 'promociones.altaPromocion',
+    mixins: [CallPromiseMixin],
+    validate: Promociones.simpleSchema().pick(CAMPOS_PROMOCIONES).validator({
+        clean: true,
+        filter: false
+    }),
+    run({nombre, descuento, precioLista, fechaInicio, fechaFin}) {
+        return Promociones.insert({nombre, descuento, precioLista, fechaInicio, fechaFin}, (err)=> {
+            if (err) {
+                throw  new Meteor.Error(500, "Error al realizar ala operación", "insertar-promo");
+            }
+        });
+    }
+});
+
+export const aplicarPromocionProductos = new ValidatedMethod({
+    name: 'promociones.aplicarProductos',
+    mixins: [CallPromiseMixin],
+    validate: new SimpleSchema({
+        factorNuevoId: {type: String},
+        productos: {type: [Object], blackbox: true}
+    }).validator(),
+    run({factorNuevoId, productos}) {
+        console.log('Dentro del metodo', factorNuevoId, productos);
+        let result = [];
+        let error = [];
+        productos.forEach((prod)=> {
+            ProductosInventarios.update({_id: prod._id}, {$set: {factorId: factorNuevoId}}, (err)=> {
+                if (err) {
+                    error.push({_id: prod._id});
+                } else {
+                    result.push({_id: prod._id})
+                }
+            });
+        });
+        if (result.length === 0) {
+            throw Meteor.Error('404', 'Erro al realizar la operacion.', 'erro-aplicar-factor');
+        }
+        return result;
+    }
+});
+
+export const aplicarPromocionMarca = new ValidatedMethod({
+    name: 'promociones.aplicarMarca',
+    mixins: [CallPromiseMixin],
+    validate: new SimpleSchema({
+        tiendaId: {type: String},
+        marcaId: {type: String},
+        factorNuevoId: {type: String},
+        excepciones: {type: [Object], blackbox: true}
+    }).validator(),
+    run({tiendaId, marcaId, factorNuevoId, excepciones}) {
+        if (Meteor.isServer) {
+            let arrIds = [];
+            excepciones.forEach((prod)=> {
+                arrIds.push(prod._id);
+            });
+            let prodInventariosBulk = ProductosInventarios.rawCollection().initializeUnorderedBulkOp();
+            const selector = {
+                $and: [
+                    {_id: {$nin: arrIds}},
+                    {tiendaId: tiendaId},
+                    {marcaId: marcaId}
+                ]
+            };
+            prodInventariosBulk.find(selector).update({$set: {factorId: factorNuevoId}});
+            const execute = Meteor.wrapAsync(prodInventariosBulk.execute, prodInventariosBulk);
+            try {
+                execute();
+                BitaFactoresPromo.insert({
+                    usuarioId: this.userId, operacion: 'aplica-factor',
+                    marcaId: marcaId, excepciones: arrIds
+                });
+                return true;
+            } catch (error) {
+                throw new Meteor.Error(403, 'Error al aplicar factor', 'error.aplicar-factor');
+            }
+        }
+    }
+});
+
+
+const PROMOCIONES_METHODS = _.pluck([altaPromocion, aplicarPromocionProductos, aplicarPromocionMarca], 'name');
+if (Meteor.isServer) {
+    DDPRateLimiter.addRule({
+        name(name) {
+            return _.contains(PROMOCIONES_METHODS, name);
+        },
+        connectionId() {
+            return true;
+        },
+    }, 5, 1000);
+}
