a4535eb6eec47474f210a91e9846fbfa2e63a902
hEctor <hector.kaizen@gmail.com>
2017-07-24 17:03:20 -0500

    Se elimina un console

--- imports/api/direcciones/server/publish.js
+++ imports/api/direcciones/server/publish.js
@@ -6,7 +6,6 @@ import {Direcciones} from "../collection";
 
 if (Meteor.isServer) {
     Meteor.publish('direcciones.todas', function (filter) {
-        console.log('[12] Veremos que llega en filter', filter);
         if (Object.keys(filter).length === 0 && filter.constructor === Object) {
             this.ready();
         } else {
