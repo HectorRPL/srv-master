48b2e6309dd4d2b5644355ffe10a61bdf5ee1186
hEctor <hector.kaizen@gmail.com>
2017-09-26 16:46:24 -0500

    [FIXTURES] Se limpia el fixtures

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -5,19 +5,3 @@ import "./fixtures/proveedores/proveedores";
 import "./fixtures/productos/helvex";
 import "./fixtures/productos/daltile";
 import "./fixtures/inventarios/inventarios";
-
-Meteor.startup(function () {
-/*
-    if (PuestosRoles.find().count() === 0) {
-        const roles = [
-            {"name": "crea_factores"},
-            {"name": "actu_factores"},
-            {"name": "borr_factores"}
-        ];
-
-        roles.forEach((rol) => {
-            Meteor.roles.insert(rol);
-        });
-    }
-*/
-});
