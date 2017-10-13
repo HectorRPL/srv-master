d095f2ae03181994bdf1a9e571d01f926f233b06
hEctor <hector.kaizen@gmail.com>
2017-10-13 01:17:27 -0500

    [datos fiscales] [provedores] [BUG] Ya se insertan/editan datos fiscles Hay problma cuando existen datos fiscales pero no la dirección.

--- imports/ui/components/proveedores/editarProveedor/editarProveedorDatosFiscales/editarProveedorDatosFiscales.js
+++ imports/ui/components/proveedores/editarProveedor/editarProveedorDatosFiscales/editarProveedorDatosFiscales.js
@@ -1,10 +1,13 @@
 /**
  * Created by Héctor on 25/07/2017.
  */
-import {DatosFiscales} from "../../../../../api/datosFiscales/collection";
-import {name as AltaDatosFiscales} from "../../../comun/altasCambios/datosFiscales/altaDatosFiscales/altaDatosFiscales";
-import {name as CambiosDatosFiscales} from "../../../comun/altasCambios/datosFiscales/cambiosDatosFiscales/cambiosDatosFiscales";
-import template from "./editarProveedorDatosFiscales.html";
+import {DatosFiscales}              from "../../../../../api/datosFiscales/collection";
+import {crearDatoFiscal}            from "../../../../../api/datosFiscales/methods";
+import {actlzrDireccDatsFiscls}     from "../../../../../api/datosFiscales/methods";
+import {name as Alertas}            from "../../../comun/alertas/alertas";
+import {name as FormaDireccion}     from "../../../comun/formas/formaDireccion/formaDireccion";
+import {name as FormaDatosFiscales} from "../../../comun/formas/formaDatosFiscales/formaDatosFiscales";
+import template                     from "./editarProveedorDatosFiscales.html";
 
 class EditarProveedorDatosFiscales {
     constructor($scope, $reactive, $stateParams) {
@@ -12,35 +15,60 @@ class EditarProveedorDatosFiscales {
         this.$scope = $scope;
         $reactive(this).attach($scope);
 
+        this.direccion = {};
+
         this.propietarioId = $stateParams.proveedorId;
 
         this.tipoMsj = '';
 
-        this.muestrarDatosFiscales = false;
-        this.mostrarCampos = false;
-
         this.subscribe('datosFiscales.proveedor', () => [{propietarioId: this.propietarioId}]);
         this.helpers({
-            datosFiscalesActuales(){
+            datosFiscales(){
                 return DatosFiscales.findOne({propietarioId: this.propietarioId});
             }
         });
-
     }
 
-    editar() {
-        this.datosFiscales = angular.copy(this.datosFiscalesActuales);
-        this.mostrarCampos = true;
+    altaDatosFiscales() {
+        let datosFiscalesFinal = angular.copy(this.datosFiscales);
+        delete datosFiscalesFinal.colonias;
+        delete datosFiscalesFinal.fechaCreacion;
+        datosFiscalesFinal.propietarioId = this.propietarioId;
+
+        crearDatoFiscal.callPromise(datosFiscalesFinal).then(this.$bindToContext(() => {
+            this.tipoMsj = 'success';
+        })).catch(this.$bindToContext((err) => {
+            this.tipoMsj = 'danger';
+        }));
     }
 
+    actualizarDireccionFiscal() {
+        delete this.datosFiscales.colonias;
+        delete this.datosFiscales._id;
+        delete this.datosFiscales.rfc;
+        delete this.datosFiscales.tipoPersona;
+        delete this.datosFiscales.razonSocial;
+        delete this.datosFiscales.tipoSociedad;
+        delete this.datosFiscales.apellidos;
+        delete this.datosFiscales.nombres;
+        delete this.datosFiscales.email;
+        delete this.datosFiscales.fechaCreacion;
+
+        actlzrDireccDatsFiscls.callPromise(this.datosFiscales).then(this.$bindToContext(() => {
+            this.tipoMsj = 'success';
+        })).catch(this.$bindToContext((err) => {
+            this.tipoMsj = 'danger';
+        }));
+    }
 }
 
 const name = 'editarProveedorDatosFiscales';
 
 export default angular
     .module(name, [
-        AltaDatosFiscales,
-        CambiosDatosFiscales
+        Alertas,
+        FormaDireccion,
+        FormaDatosFiscales
     ])
     .component(name, {
         template: template.default,
