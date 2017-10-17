f40c9ef2b7dda38f90b4724a998bfe6dfc7648dd
hEctor <hector.kaizen@gmail.com>
2017-10-17 16:17:14 -0500

    [directive] [capitalize] Se añade la directiva a la forma 'datosFiscales'

--- imports/ui/components/app/app.js
+++ imports/ui/components/app/app.js
@@ -1,13 +1,14 @@
 /**
  * Created by Héctor on 09/02/2017.
  */
+import "metismenu/dist/metisMenu.js";
+import {name as CapitalizarInputs} from "../directives/capitalizarInputs/capitalizarInputs";
 import {name as Navigation} from "./navigation/navigation";
 import {name as Topnavbar} from "./topnavbar/topnavbar";
 import {name as ProveedoresClass} from "../proveedores/proveedores";
 import {name as TiendasClass} from "../tiendas/tiendas";
 import {name as Usuarios} from "../usuarios/usuarios";
 import {name as Marcas} from "../marcas/marcas";
-import "metismenu/dist/metisMenu.js";
 import template from "./app.html";
 
 class App {
@@ -23,6 +24,7 @@ const name = 'app';
 
 export default angular
     .module(name, [
+        CapitalizarInputs,
         Navigation,
         Topnavbar,
         ProveedoresClass,
